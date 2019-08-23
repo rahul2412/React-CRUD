b451c965af3584e4d1e683f15f99e395bef7e9bb
Rahul Passi <rahulpassi2412@gmail.com>
2019-08-23 11:57:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Data from './data';
+
+
+function App() 
+ {
+  return (
+    <div>
+   <h1> React CRUD </h1>
+   <Data/>
+  </div>);
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,179 @@
+import React from 'react';
+
+class Data extends React.Component {
+
+  constructor(props){ 
+  
+  super(props);
+  
+  this.state={ id_new:"", name_new:"", skill_new:"", 
+  data: [{id:0, name: "Rohan", skill: "Testing"},
+  {id:1, name: "Raghav", skill: "Vue.js"},
+  {id:2, name: "Sachin", skill: "Ruby on rails"},
+  {id:3, name: "Ritesh", skill: "IOT"}]};
+  
+  this.removeIt = this.removeIt.bind(this);
+  
+  this.updateIt = this.updateIt.bind(this);
+  
+  this.createIt = this.createIt.bind(this);
+  
+  this.handleChangeId = this.handleChangeId.bind(this);
+  
+  this.handleChangeName = this.handleChangeName.bind(this);
+  
+  this.handleChangeSkill = this.handleChangeSkill.bind(this);
+  
+  
+  }
+  
+  async removeIt(name,id) {
+  
+  
+  console.log(name+ " deleted");
+  
+  /*
+  
+  this.setState({
+  
+  data: this.state.data.filter((_, i) => _.name !== name)
+  
+  });
+  */
+ //Alternate way but discouraged
+
+  
+  await this.setState((prevState) => ({
+  
+  data: prevState.data.filter((_, i) => _.id !== id)
+  
+  }));
+  
+  
+  console.log(this.state.data);
+  
+  }
+  
+  
+  async updateIt(name) {
+  
+  let new_skill = prompt("Enter Skills for "+name);
+  
+  //console.log(name+"- "+new_skill);
+  
+  
+  let old_data=this.state.data;
+  
+  
+  let new_data= await old_data.map(item=>{
+  
+  if(item.name===name){
+  
+  return {...item,skill:new_skill}
+  
+  }
+  
+  else return item;
+  
+  })
+  
+  
+  this.setState({data:new_data});
+  
+  
+  console.log(this.state.data);
+  
+  
+  }
+  
+  createIt(e){
+  
+  e.preventDefault();
+  
+  let new_item={id:this.state.id_new, name:this.state.name_new, skill:this.state.skill_new};
+  
+  this.setState((prevState) => {
+  
+  return {
+  
+  data: prevState.data.concat(new_item)
+  
+  };
+  
+  });
+  
+  this.setState({id_new:"", name_new:"", skill_new:""});
+  
+  }
+  
+  handleChangeId(e){
+  
+  this.setState({id_new:e.target.value});
+  
+  }
+  
+  handleChangeName(e){
+  
+  this.setState({name_new:e.target.value});
+  
+  }
+  
+  handleChangeSkill(e){
+  
+  this.setState({skill_new:e.target.value});
+  
+  }
+  
+  render() {
+  
+  let list=this.state.data;
+  
+  return(
+  
+  <div>
+  
+  
+  <br/>
+  
+  <form onSubmit={this.createIt}>
+  
+  
+  <input name="id" required type="text" value={this.state.data_form} onChange={this.handleChangeId} placeholder="Enter id" />
+  
+  <input name="name" required type="text" value={this.state.data_form} onChange={this.handleChangeName} placeholder="Enter name" />
+  
+  <input name="skill" required type="text" value={this.state.data_form} onChange={this.handleChangeSkill} placeholder="Enter skill" />
+  
+  <input type="submit" value="Submit"/>
+  
+  </form>
+  
+  <ul>
+  
+  {list.map(item => (
+  
+  <li key={item.id}>
+  
+  <div>{item.id}</div>
+  
+  <div>{item.name}</div>
+  
+  <div>{item.skill}</div>
+  
+  <button onClick={() => {this.removeIt(item.name, item.id) }}>Delete</button>
+  
+  <button onClick={() => {this.updateIt(item.name) }}>Update</button>
+  
+  <br/><br/></li>
+  
+  ))}
+  
+  </ul></div>
+  
+  )
+
+  }
+  
+  }
+export default Data;
+
