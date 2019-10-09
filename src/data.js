bfe842fe968e186d9bbb3cdfa4b36ac46c420618
Rahul Passi <rahulpassi2412@gmail.com>
2019-10-09 16:43:00 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -164,7 +164,7 @@ this.setState({id_new:"", name_new:"", skill_new:""});
   
   <input name="skill" required type="text" value={this.state.skill_new} onChange={this.handleChangeSkill} placeholder="Enter skill" />
   
-  <span><Button variant="success" type="submit">Create</Button></span>
+  <Button style={{margin:"10px"}} variant="success" type="submit">Create</Button>
   
   </form>
   <br/>
